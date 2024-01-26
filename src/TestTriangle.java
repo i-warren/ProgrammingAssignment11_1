cf4cca19f1c2353aefec42488d82daf1ada59fba
Isaac Warren <isaacwarren25@gmail.com>
2024-01-26 13:07:47 -0500

    Created TestTriangle.java Commented both Triangle.java and TestTriangle.java

--- src/TestTriangle.java
+++ src/TestTriangle.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * TestTriangle has the user enter the length of three sides of
+ * a triangle, the triangle color, and a boolean value to indicate
+ * whether the triangle is filled. It then creates a Triangle object
+ * using the input as the parameters
+ * TestTriangle then displays the toString of the object as well as
+ * the area, perimeter, and whether the triangle is filled.
+ *
+ * @author Isaac Warren
+ */
 public class TestTriangle {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);  // Creates new Scanner Object
+
+        // Prompt user to enter three sides of a Triangle
+        System.out.print("Enter three sides of a triangle: ");
+        double side1 = input.nextDouble();
+        double side2 = input.nextDouble();
+        double side3 = input.nextDouble();
+
+        // Prompt user to enter a color
+        System.out.println("\nEnter a color: ");
+        String color = input.next();
+
+        // Prompt user to enter whether triangle is filled
+        System.out.println("\nIs the triangle filled ('true' or 'false'? ");
+        boolean filled = input.nextBoolean();
+
+        // Create Triangle object myTriangle from user defined parameters
+        Triangle myTriangle = new Triangle(side1, side2, side3);
+        myTriangle.setColor(color);
+        myTriangle.setFilled(filled);
+
+        // Output myTriangle info
+        System.out.println("\n\n");
+        System.out.println(myTriangle.toString());
+        System.out.println("Area: " + myTriangle.getArea());
+        System.out.println("Perimeter: " + myTriangle.getPerimeter());
+        System.out.println("Color: " + myTriangle.getColor());
+        System.out.println("Triangle is " +
+                (myTriangle.isFilled() ? "" : "not ")
+                + "filled");
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,39 +1,57 @@
-public class Triangle extends GeometricObject{
+/**
+ * Triangle is a java class to describe a geometric triangle
+ * It extends the GeometricObject class
+ * Triangle has two constructors:
+ * Triangle(), Triangle(double side1, double side2, double side3)
+ *
+ * @author Isaac Warren
+ */
+public class Triangle
+        extends GeometricObject{
 
     private double side1;
     private double side2;
     private double side3;
+
+    /**Default construct*/
     Triangle() {
         side1 = side2 = side3 = 1.0;
     }
 
+    /** Construct a Triangle from user input sides*/
     Triangle(double side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
     }
 
+    /** Getter method for side1 */
     public double getSide1() {
         return side1;
     }
 
+    /** Getter method for side2 */
     public double getSide2() {
         return side2;
     }
 
+    /** Getter method for side3 */
     public double getSide3() {
         return side3;
     }
 
+    /** Getter method for area. Overrides GeometricObject method */
     public double getArea() {
         double s = (side1 + side2 + side3) / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
 
+    /** Getter method for perimeter. Overrides GeometricObject method */
     public double getPerimeter() {
         return side1 + side2 + side3;
     }
 
+    /** Overrides toString. Returns a String describing Triangle */
     public String toString() {
         return "Triangle: side1 = " + side1 + " side2 = " + side2 +
                 " side3 = " + side3;
